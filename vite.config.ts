91d0337d8733050474b134da75a7946dadde29d2
shaitools891-svg <shaitools891@gmail.com>
2025-08-22 22:36:18 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
-// vite.config.ts
-import {{"{"}} defineConfig {{"}"}} from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({{"{"}}
+export default defineConfig({
   plugins: [react()],
   base: '/bright-hearts-animation/', // Replace with your repository name
-  build: {{"{"}}
+  build: {
     outDir: 'dist'
-  {{"}"}}
-{{"}"}}) 
+  }
+})
