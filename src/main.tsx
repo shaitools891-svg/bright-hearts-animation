69ac7cf31c3dd430f63bcd3569df6dae932c4cb4
shaitools891-svg <shaitools891@gmail.com>
2025-08-23 09:53:11 +0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import './i18n'; // Add this import
+import './i18n'; // Make sure this import exists
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
